c2b1f1e816974a4d7b74962dad872eeeaacfed56
H.T. Koo <h1017760@connect.hku.hk>
2017-06-13 09:12:03 +0800

    FreeCodeCamp - Back End Development Certification - API projects: URL_Shortener_Microservice - updated API usage page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-var requestHeaderParser = require("../service/requestHeaderParser");
+var timestampParser = require("../service/timestampParser");
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.send(requestHeaderParser(req));
+    res.render('index', {
+        "serverHostName": req.headers.host
+    });
+});
+
+router.get('/:timestamp', function (req, res) {
+    var timestamp = req.params.timestamp;
+    res.send(timestampParser(timestamp));
 });
 
 module.exports = router;
