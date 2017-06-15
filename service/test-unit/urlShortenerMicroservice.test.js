58ec4e46acfe2efb3ea1a4f58cf6ba75e41dd916
H.T. Koo <h1017760@connect.hku.hk>
2017-06-15 21:18:20 +0800

    FreeCodeCamp - Back End Development Certification - API projects: URL_Shortener_Microservice - added dummy response for tryShortening API

--- service/test-unit/urlShortenerMicroservice.test.js
+++ service/test-unit/urlShortenerMicroservice.test.js
@@ -2,6 +2,7 @@
  * Created by Hey on 13 Jun 2017
  */
 var test = require('chai');
+var format = require('string-format');
 
 var urlShortenerMicroservice = require('../urlShortenerMicroservice');
 
@@ -13,5 +14,19 @@ describe("urlShortenerMicroservice", function () {
 
         //    then
         test.expect(mongoUrl).to.be.not.undefined;
+    });
+    describe("Shortening URL", function () {
+        it("should throw error for invalid URL that does not follow the valid http://www.example.com format", function () {
+            //    given
+            var anInvalidUrl = "some invalid url";
+
+            //    when
+            var jsonResponse = urlShortenerMicroservice.tryShortening(anInvalidUrl);
+
+            //    then
+            test.expect(jsonResponse.error).to.equal(format("'{}' is not a valid url that follow the format 'http://www.example.com'", anInvalidUrl));
+            test.expect('shortened_from' in jsonResponse).to.be.false;
+            test.expect('shortened_to' in jsonResponse).to.be.false;
+        });
     });
 });
--- service/urlShortenerMicroservice.js
+++ service/urlShortenerMicroservice.js
@@ -3,5 +3,10 @@
  */
 require('dotenv').config();
 
-module.exports = function (request) {
+module.exports = {
+    tryShortening: function(){
+        return {
+            "error": "'some invalid url' is not a valid url that follow the format 'http://www.example.com'"
+        }
+    }
 };
