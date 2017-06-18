1bb8a777a1e1d3a1a3df5d5a509ddfd2d94645c5
H.T. Koo <h1017760@connect.hku.hk>
2017-06-18 16:25:11 +0800

    FreeCodeCamp - Back End Development Certification - API projects: URL_Shortener_Microservice - added functionality to return existing if found when persisting

--- service/shortenedUrlPersister.js
+++ service/shortenedUrlPersister.js
@@ -9,18 +9,34 @@ var idGenerator = require('./idGenerator');
 var MONGO_URL = process.env.MONGO_URL;
 var COLLECTION_NAME_SHORTEN_URL = 'shortenedUrls';
 
+function alignFindResultToBInsertResultFormat(data) {
+    return {
+        ops: data
+    };
+}
+
 module.exports = {
     "getPromiseFor": {
-        "persist": function (oriUrl) {
+        "persistOrReturnExisting": function (oriUrl) {
+            var collection;
             return mongo.connect(MONGO_URL)
                 .then(function (db) {
-                    return db.collection(COLLECTION_NAME_SHORTEN_URL)
-                        .insert({
-                            'shorten_from': oriUrl,
-                            'shorten_to': idGenerator.generate()
-                        }).then(function (data) {
-                            return data.ops[0]["shorten_to"];
-                        });
+                    collection = db.collection(COLLECTION_NAME_SHORTEN_URL);
+                    return collection.find({
+                        'shorten_from': oriUrl
+                    }).toArray();
+                })
+                .then(function (data) {
+                    if (data.length > 0) {
+                        return data[0]["shorten_to"];
+                    }
+
+                    return collection.insert({
+                        'shorten_from': oriUrl,
+                        'shorten_to': idGenerator.generate()
+                    }).then(function (data) {
+                        return data.ops[0]["shorten_to"];
+                    });
                 })
                 .catch(function (err) {
                     console.log("Error caught for mongo db connections: " + err);
--- service/test-unit/shortenedUrlPersister.test.js
+++ service/test-unit/shortenedUrlPersister.test.js
@@ -18,7 +18,7 @@ var shortenedUrlPersister = rewire('../shortenedUrlPersister');
 shortenedUrlPersister.__set__('mongo', mockMongoClient);
 
 describe("shortenedUrlPersister", function () {
-    describe("Usage of dotenv - process.env.MONGO_URL", function(){
+    describe("Usage of dotenv - process.env.MONGO_URL", function () {
         it("should be able to get MONGO_URL from .env", function () {
             //    given
             //    when
@@ -29,7 +29,7 @@ describe("shortenedUrlPersister", function () {
         });
     });
 
-    describe("persist", function(){
+    describe("persist", function () {
         it("should be able to persist unique original url", function () {
             //    given
             var mongoUrl = process.env.MONGO_URL;
@@ -39,7 +39,7 @@ describe("shortenedUrlPersister", function () {
             stub_IdGenerator.returns(shortenedUrl);
 
             //    when
-            var promise = shortenedUrlPersister.getPromiseFor.persist(originalUrl);
+            var promise = shortenedUrlPersister.getPromiseFor.persistOrReturnExisting(originalUrl);
 
             //    then
             var handlerForCleanUp = {};
@@ -64,6 +64,109 @@ describe("shortenedUrlPersister", function () {
                 stub_IdGenerator.restore();
             }).catch(function (err) {
                 throw err;
+            });
+        });
+
+        it("should return existing shortened url if found instead of generating new when persist", function () {
+            //    given
+            var mongoUrl = process.env.MONGO_URL;
+            var originalUrl = "originalUrl";
+            var shortenedUrl = "short";
+            var stub_IdGenerator = sinon.stub(idGenerator, "generate");
+            stub_IdGenerator.throws("Unexpected call of idGenerator.generate()");
+
+            var handlerForCleanUp = {};
+            return mockMongoClient.connect(mongoUrl)
+                .then(function (db) {
+                    handlerForCleanUp.db = db;
+                    var collection = db.collection('shortenedUrls');
+                    handlerForCleanUp.collection = collection;
+
+                    var existingEntry = {"shorten_from": originalUrl, "shorten_to": shortenedUrl};
+                    return collection.insert(existingEntry);
+
+                    /*
+                     handlerForCleanUp.collection.find({"shorten_from": originalUrl}).toArray().then(function (data) {
+                     console.log(data);
+                     });
+                     * */
+                    // collection.find({"a":1}).toArray().then(function(data){console.log(data)})
+                    // var state = collection.toJSON();
+                    // if (typeof state.documents === "undefined") {
+                    //     state.documents = {};
+                    // }
+                    // state.documents.push({"shorten_from": originalUrl, "shorten_to": shortenedUrl});
+                }).then(function (data) {
+                    //    when
+                    return shortenedUrlPersister.getPromiseFor.persistOrReturnExisting(originalUrl);
+                }).then(function (returnedUrl) {
+                    //    then
+                    test.expect(returnedUrl).to.be.equal(shortenedUrl);
+                    return mockMongoClient.connect(mongoUrl);
+                }).then(function (db) {
+                    handlerForCleanUp.db = db;
+
+                    var collection = db.collection('shortenedUrls');
+                    handlerForCleanUp.collection = collection;
+                    return collection.findOne({
+                        "shorten_from": originalUrl
+                    })
+                }).then(function (data) {
+                    test.expect(data["shorten_to"]).to.equal(shortenedUrl);
+                }).then(function () {
+                    // truncate
+                    handlerForCleanUp.collection.toJSON().documents.length = 0;
+                    handlerForCleanUp.db.close();
+                    stub_IdGenerator.restore();
+                }).catch(function (err) {
+                    throw err;
+                });
+        });
+    });
+
+    it("temp", function (done) {
+        // Connection URL
+        var url = 'mongodb://localhost:27017/myproject';
+// Use connect method to connect to the Server
+        mockMongoClient.connect(url, {}, function (err, db) {
+            // Get the documents collection
+            var collection = db.collection('documents');
+            // Insert some documents
+            var docs = [{a: 1}, {a: 2}, {a: 3}];
+            collection.insert(docs, function (err, result) {
+                console.log('inserted', result);
+
+                collection.update({a: 2}, {$set: {b: 1}}, function (err, result) {
+                    console.log('updated', result);
+
+                    collection.findOne({a: 2}, {b: 1}, function (err, doc) {
+                        console.log('foundOne', doc);
+
+                        collection.remove({a: 3}, function (err, result) {
+                            console.log('removed', result);
+
+                            collection.find({}, {_id: -1}).toArray(function (err, docs) {
+                                console.log('found', docs);
+
+                                function cleanup() {
+                                    var state = collection.toJSON();
+                                    // Do whatever you want. It's just an Array of Objects.
+                                    state.documents.push({a: 2});
+
+                                    // truncate
+                                    state.documents.length = 0;
+
+                                    // closing connection
+                                    db.close();
+
+                                    done();
+                                }
+
+                                setTimeout(cleanup, 1000);
+                            });
+                        });
+                    });
+                });
             });
         });
     });
