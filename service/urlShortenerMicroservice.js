5e8af56985bd0d6bfd5090c495ab2cb060d60cf3
H.T. Koo <h1017760@connect.hku.hk>
2017-08-11 19:25:03 +0800

    URL_Shortener_Microservice - refactored urlShortenerMicroservice.js - from FreeCodeCamp - Back End Development Certification - API projects

--- service/urlShortenerMicroservice.js
+++ service/urlShortenerMicroservice.js
@@ -9,22 +9,26 @@ function createErrorMessage(possibleUrl) {
     return "'" + possibleUrl + "' is not a valid url that follow the format 'http://www.example.com'"
 }
 
+function promiseForShortening(possibleUrl, fullHostName) {
+    return shortenedUrlPersister.getPromiseFor
+        .persistOrReturnExisting(possibleUrl, fullHostName)
+        .then(function (shortenedUrl) {
+            return {
+                "shortened_from": possibleUrl,
+                "shortened_to": shortenedUrl
+            }
+        })
+        .catch(function (error) {
+            return {
+                'error': 'Unable to shorten url, reason: '.concat(error.message)
+            }
+        });
+}
+
 module.exports = {
     tryShortening: function (possibleUrl, fullHostName) {
         if (validUrl.isWebUri(possibleUrl)) {
-            return shortenedUrlPersister.getPromiseFor
-                .persistOrReturnExisting(possibleUrl, fullHostName)
-                .then(function (shortenedUrl) {
-                    return {
-                        "shortened_from": possibleUrl,
-                        "shortened_to": shortenedUrl
-                    }
-                })
-                .catch(function (error) {
-                    return {
-                        'error': 'Unable to shorten url, reason: '.concat(error.message)
-                    }
-                });
+            return promiseForShortening(possibleUrl, fullHostName);
         } else {
             return Promise.resolve({
                 "error": createErrorMessage(possibleUrl)
